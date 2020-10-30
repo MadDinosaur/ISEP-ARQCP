1b35fc14617f82305963d4bddf32faecb8d888c4
1191430 <1191430@isep.ipp.pt>
2020-10-30 14:04:05 +0000

    Agora sim, o exercício 15 ficou funcional

--- modulo2/ex15/compute.s
+++ modulo2/ex15/compute.s
@@ -19,11 +19,27 @@ compute:
 	mul %ecx
 	
 	addl C,%eax
-	
+
 	movl D,%ecx
 	
-	divl %ecx
+	cmp $0,%ecx
+	je verificar_zero
 	
+	cdq
+	
+	idivl %ecx
+	
+	adcl $0,%edx
+	
+	#epilogue
+	movl %ebp, %esp
+	popl %ebp
+	ret
+	
+verificar_zero:
+
+	movl $0, %eax
+
 	#epilogue
 	movl %ebp, %esp
 	popl %ebp
--- modulo2/ex15/main.c
+++ modulo2/ex15/main.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 #include "compute.h"
 
-int A=2,B=3,C=4,D=2;
+int A=8000,B=-8000,C=40,D=-10;
 
 int main()
 {
 	int res = (int) compute();
-		
-	printf("O resultado da operação é: %d \n", res);
+	printf("O resultado da operação é: %d \n", res);	
 	
 	return 0;
 }
